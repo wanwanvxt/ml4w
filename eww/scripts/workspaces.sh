a952a9fef8a877499fa0cfe55d5bbb94e1971653
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-06-29 22:38:37 +0700

    start new theme layout and styling for `eww`

--- eww/scripts/workspaces.sh
+++ eww/scripts/workspaces.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
 workspaces() {
-  WORKSPACES=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-  seq 1 10 | jq -Mc --argjson windows "${WORKSPACES}" --slurp 'map({id: ., windows: ($windows[. | tostring]//0)})'
+  local workspaces_json=$(hyprctl workspaces -j)
+  local active_id=$(hyprctl activeworkspace -j | jq -r '.id')
+
+  local workspaces=$(echo "$workspaces_json" | jq 'map({key: (.id | tostring), value: .}) | from_entries')
+
+  seq 1 10 | jq -Mc --argjson map "$workspaces" --argjson active_id "$active_id" --slurp '
+    map(
+      ($map[. | tostring] // {
+        id: .,
+        name: (. | tostring),
+        monitor: null,
+        monitorID: 0,
+        windows: 0,
+        hasfullscreen: false,
+        lastwindow: null,
+        lastwindowtitle: null,
+        ispersistent: false
+      }) + {active: (. == $active_id)}
+    )
+  '
 }
 
-active_workspace() {
-  hyprctl activeworkspace -j | jq -Mc '{id: .id, lastwindowtitle: .lastwindowtitle}'
-}
-
-run() {
-  if [[ "$1" == "--active" ]]; then
-    active_workspace
-  else
-    workspaces
-  fi
-}
-
-run "$1"
+workspaces
 socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-  run "$1"
+  workspaces
 done
