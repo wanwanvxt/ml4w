41d3d976cbe80b0e5b1f1f66e3079d1778fd3e4d
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2025-07-26 20:22:58 +0700

    ensure rsync doesn't delete other configs

--- share/dotfiles/quickshell/install.sh
+++ share/dotfiles/quickshell/install.sh
@@ -1,1 +1,1 @@
-rsync -av --exclude "/install.sh" --exclude ".qmlls.ini" "$dotfiles_dir/quickshell/" "$user_config_dir/quickshell/"
+rsync -av --delete --exclude "/install.sh" --exclude ".qmlls.ini" "$dotfiles_dir/quickshell/megumi" "$user_config_dir/quickshell/megumi"
